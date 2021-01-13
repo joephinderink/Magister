f9fc5b16ae78fe316324c5d9eb0cddd7fd81af54
Joep Hinderink <hello@joephinderink.nl>
2021-01-13 10:07:18 +0100

    Initial Commit

---
+++ Magister/MagisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagisterApp.swift
+//  Magister
+//
+//  Created by Joep Hinderink on 13/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MagisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
